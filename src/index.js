9ad1c4787dc20720141e04538a81a3f2fdef6932
hyuk <woo1038@hanmail.net>
2021-07-27 14:56:57 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+
+function def(person) {
+  return person.name + " " + person.age;
+}
+
+const person = {
+  name: "hleo",
+  age: 16
+}
+const element = <h1>Hello, {def(person)}!</h1>
+
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */
+ReactDOM.render(
+  element,
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
